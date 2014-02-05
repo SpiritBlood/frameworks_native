a281fab10517d21b227d9f2cd19f1a12df98d7c7
nuclearmistake <nuclearmistake@gmail.com>
2014-02-05 12:42:07 -0500

    surfaceflinger: Properly fix aliasing violations
    Change-Id: I5eaaa57665a2b34eea9d42e0df77c6747332b04e

--- services/surfaceflinger/DisplayHardware/HWComposer.cpp
+++ services/surfaceflinger/DisplayHardware/HWComposer.cpp
@@ -1131,7 +1131,12 @@ public:
         getLayer()->transform = transform;
     }
     virtual void setFrame(const Rect& frame) {
-        reinterpret_cast<Rect&>(getLayer()->displayFrame) = frame;
+        union {
+            hwc_rect_t* displayFrame;
+            const Rect* inframe;
+        };
+        inframe = &frame;
+        getLayer()->displayFrame = *displayFrame;
     }
     virtual void setCrop(const FloatRect& crop) {
         /*
